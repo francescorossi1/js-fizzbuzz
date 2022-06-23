eee4336badea35e6f81d22737b728c0ac6d3029c
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-06-23 15:29:41 +0200

    console display completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 console.log('JS OK')
+
+// Steps
+
+const display = document.getElementById('display');
+let printNumber = ""
+
+// Stampo in console i numeri da 1 a 100
+
+for(let i = 1; i <= 100; i++){
+
+    // Se il modulo restituisce 0 (è divisibile per) associo la parola
+    if(i % 5 == 0 && i % 3 == 0){
+        printNumber = ' fizzbuzz';     
+    }
+    else if(i % 3 == 0){
+        printNumber = ' fizz';    
+    }
+    else if(i % 5 == 0){
+        printNumber = ' buzz';    
+    }
+
+    // Altrimenti associo il numero attuale
+    else{
+        printNumber = ` ${i}`;    
+    }
+    console.log(printNumber)
+    
+}
+
